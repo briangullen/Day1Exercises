2a75175448a77ebf7d62110a8bdbd9891c2b3237
Brian Gullen <brian.gullen@gmail.com>
2024-05-01 21:30:51 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Exercise 1 - Echo String
+your_name = input('>>What is your name? ')
+print('>> Hello ' + your_name)
+
+# Exercise 2 - Adding a number to an int
+given_number = int(input('Please enter a number: '))
+print(f"Your number is: {given_number}")
+
+# Exercise 3 - Adding a number to a float
+prefloat_number = int(input('Please provide a number: '))
+postfloat_number = prefloat_number + .5
+print(f"Your number plus 0.5 is {postfloat_number}")
+
+# Exercise 4 -  Adding 2 floats
+number_one = float(input('Please enter a number with a decimal: '))
+number_two = float(input('Please enter another number with a decimal: '))
+total_addition = number_one + number_two
+print(f"Your numbers added together equal: {total_addition}")
+
+# Exercise 5 - Multiplying floats
+multiple_one = float(input('Please provide a number with a decimal: '))
+multiple_two = float(input('Please provide another number with a decimal: '))
+multiple_total = multiple_one * multiple_two
+print(f"Your numbers multiplied equal: {multiple_total}")
+
+# Exercise 6 - Dividing Integers
+division_one = int(input('Please provide a whole number: '))
+division_two = int(input('Please provide another whole number: '))
+division_total = int(division_one / division_two)
+print(f"Your numbers divided by each other equal: {division_total}")
+
+
